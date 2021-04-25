fce027b33c99baf2582371ab19fa508623089559
Itay <eizzo77@gmail.com>
2021-04-26 00:25:31 +0300

    another try

--- client/src/db/mongoose.js
+++ client/src/db/mongoose.js
@@ -7,12 +7,19 @@ const atlasURL =
   "mongodb+srv://ItayZaguri:togZzih3VqZBqthG@cluster0.df8aj.mongodb.net/Bank-Api?retryWrites=true&w=majority";
 console.log("trying to CONNECt");
 console.log("in mongoose");
-mongoose
-  .connect(atlasURL || "mongodb://127.0.0.1:27017/Bank-Api", {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("connected to MongoDBAtlas"))
-  .catch((error) => console.log(error));
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(atlasURL || "mongodb://127.0.0.1:27017/Bank-Api", {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    console.log("connected To Mongo");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = connectDB;
