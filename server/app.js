78aa63ebb860eb8c4eb208219087011958c25f3a
Itay <eizzo77@gmail.com>
2021-04-26 09:42:23 +0300

    some reorderings

--- server/app.js
+++ server/app.js
@@ -12,6 +12,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 connectDB().then(() =>
   app.listen(PORT, () => {
     console.log(`Listening on port #${PORT}`);
@@ -181,7 +185,3 @@ app.get("/api/users", async (req, res) => {
 //     res.status(404).send({ error: error.message });
 //   }
 // });
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
